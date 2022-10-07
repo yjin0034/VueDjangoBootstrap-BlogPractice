6b3800f2fc4e330237b65cb8ef8a0e939619c082
yjin0034 <yjin0034@naver.com>
2022-10-07 15:43:26 +0900

    Bootstrap-ScrollSpy 기능 적용을 위한 코드 수정(templates/blog/base.html)

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -30,11 +30,14 @@ window.addEventListener('DOMContentLoaded', event => {
     document.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
+    // scrollspy : 스크롤이 특정 위치를 지나갈 때 메뉴를 하이라이트 시키는 기능
+    // body 영역을 대상으로 감시하며, id가 mainNav인 부분(id="mainNav")에서 <a> 태그를 찾아 scollspy를 적용시킴
+    // 이때, <a> 태그 내에 있는 href 속성이나 data-bs-target 속성에 지정되어 있는 링크를 타켓으로 함.
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            offset: 74,
+            offset: 20,  // 스크롤 위치 지정. 스크롤이 20 pixel 위치에 왔을 때 해당 메뉴가 하이라이트 되게 설정
         });
     };
 
